67c3c06c237223226ffcaac7b919bb07ea8e6d49
HaoLiHaiO <pinelwebdevelopment@gmail.com>
2021-03-24 18:03:48 +0200

    language prediction demo

---
+++ document2.py
@@ -0,0 +1,67 @@
+training_doc2 = """
+Dark But Just A Game
+
+It's dark but just a game
+That's what he would say to me
+The faces aren't the same
+But their stories all end tragically
+
+Sweet, but whatever, baby
+And that's the price of fame
+A tale as old as time, you'd be
+Sweet, but whatever, baby
+A pretty little fool
+To think exceptions to the rule
+Just walk around
+Like you and me this way
+
+But life is sweet, but whatever, baby
+You gotta take them for what they've got
+And while the whole world is crazy
+We're getting high in the parking lot
+
+We keep changing all the time
+The best ones lost their minds
+So I'm not gonna change
+I'll stay the same
+No rose left on the vines
+Don't even want what's mine
+Much less the fame
+It's dark but just a game
+It's dark but just a game
+
+It's dark but just a game
+So play it like a symphony
+You know our love's the same
+They'll both go down in infamy
+
+I was a pretty little thing
+And got a lot to sing, but
+Nothing came from either one but pain
+But fuck it
+
+Life is sweet, but whatever, baby
+Don't ever think it's not
+While the whole world is crazy
+We're making out in the parking lot
+
+We keep changing all the time
+The best ones lost their minds
+So I'm not gonna change
+I'll stay the same
+No rose left on the vines
+Don't even want what's mine
+Much less the fame
+It's dark but just a game
+
+We keep changing all the time
+The best ones lost their minds
+So I'm not gonna change
+I'll stay the same
+No rose left on the vines
+Don't even want what's mine
+Much less the fame
+It's dark but just a game
+It's dark but just a game
+It's dark but just...
+"""
